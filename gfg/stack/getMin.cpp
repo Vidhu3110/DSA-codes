622b247d338fccc29434c384937f8e485992ddde
vidhu3110 <Vidhusinha.vidhu3110@gmail.con>
2021-07-20 02:03:17 +0530

    commmit1

--- gfg/stack/getMin.cpp
+++ gfg/stack/getMin.cpp
@@ -26,3 +26,35 @@ in O(1) time and O(1) space tricky solution
 
 #include<bits/stdc++.h>
 using namespace std;
+struct Stack{
+    stack<int> s;
+    int min1;
+
+    void push(int x){
+        if(s.empty()){
+            min1 = x;
+        s.push(x);
+    }else if(x<=min){
+            s.push(2*x-min);
+            min1 = x;
+        }else
+            s.push(x);
+    }
+    void pop(){
+        int t = s.top();s.pop()
+        if(t<=min1){
+        int res=min1;
+       min1=2*min1-t;
+       return res;
+   }else{
+       return t;
+        }
+        }
+    int top(){
+        int t = s.top();
+        return (t<=min) ? min:t;
+    }
+    int getMin(){
+        return min1;
+    }
+};
