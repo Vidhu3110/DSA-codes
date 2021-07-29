7343d367aa8d9a82722669f78de2865d8179d9b0
vidhu3110 <Vidhusinha.vidhu3110@gmail.con>
2021-07-30 03:06:34 +0530

    sleepy af

---
+++ gfg/Graph/graph.cpp
@@ -0,0 +1,25 @@
+/*
+    graph data structure
+    2 types of graph
+        1. Directed
+        2. Undirected
+
+    1. Directed :
+    indegree and outdegree == sum of edges == |E|
+
+    max no of edges  = |V| *(|V|-1);
+
+    2. Undirected:
+    sum of degree = 2*|E|;
+        max no of edges  = (|V| *(|V|-1))/2;
+
+
+    walk = v1 v2 v4 v2
+    path = v1 v2 v4 --- no repeated edges or vertices
+    cyclic : there exist a walk that begins and end with the same vertex
+    Acyclic : does not contain a cycle
+
+
+weighted and unweigthed graphs::
+
+*/
---
+++ gfg/Graph/representgraph.cpp
@@ -0,0 +1,14 @@
+/*
+    void addEdge(vector<int> adj[] , int u , int v){
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+}
+    void print(vector<int> adj[],int v){
+        for(int i =0;i<v;i++){
+            for(auto x : adj[i]){
+                cout<<x<<" ";
+            }
+    cout<<"\n";
+    }
+}
+*/
